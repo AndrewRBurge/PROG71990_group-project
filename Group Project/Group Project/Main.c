cf4e27e45e2ffa9c4fdcd8ec92106aa7982dc16b
AndrewRBurge <burgean@sheridancollege.ca>
2025-03-26 14:20:00 -0400

    set up basic menu
    - set up basic menu to redo later

--- Group Project/Group Project/Main.c
+++ Group Project/Group Project/Main.c
@@ -1,21 +1,91 @@
 #include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
 //PROG71990 - W25 - Group 12 - Task manager
 
+/* Task
 
+ The program your group will write will be one of:
+
+ a taskmanager
+
+
+required features:
+ 1.add a new appt/task/recipe/other
+ 2.delete an existing appt/task/recipe/other
+ 3.update an existing appt/task/recipe/other
+ 4.display single appt/task/recipe/other
+ 5.display range appt/task/recipe/other
+ 6.display all appt/task/recipe/other
+ 7. search for appt/task/recipe/other
+ 8. friendly/easy to use UI(or menu system)
+ 9. save accumulated data to disk
+ 10. load accumulated data from disk
+
+
+*/
 
 
 
 int main(void) {
 
+	char LetterLabel = 'z';
+	
+	while (LetterLabel != 'f') {
+		//display menu 
+		printf("To choose a function, enter its letter label:\n");
+		printf("a) add a task\n");
+		printf("b) delete a task\n");
+		printf("c) display single task\n");
+		printf("d) display range of tasks\n");
+		printf("e) display all tasks\n");
+		printf("f) search task\n");
+		printf("g) Quit\n");
+		char numsymbols = scanf("%c", &LetterLabel);
 
+		// there hass been a error involving the input 
+		// my reserch has shown to remove any extra space when inputing values we can us getchar() to only read the letter inputed
+		// https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2013/x198c66c(v=vs.120)
+		getchar();
 
+		if (LetterLabel == 'a') {
+			//a)add a task
+			
+		}
 
+		if (LetterLabel == 'b') {
+			//b) delete a task
+		}
 
+		if (LetterLabel == 'c') {
+			//c)display single task
 
+		}
 
+		if (LetterLabel == 'd') {
+			//d) display range of tasks
+			
+		}
 
+		if (LetterLabel == 'e') {
+			// e) display all tasks
 
+		}
 
+		if (LetterLabel == 'f') {
+			// f) search tasks
+
+		}
+
+		if (LetterLabel == 'g') {
+			// g) Quit
+			printf("quiting program");
+
+			exit(0);
+
+		}
+
+	} 
 
 }
