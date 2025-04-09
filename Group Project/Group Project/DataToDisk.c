0e202f560a03115650d66800040964e1bfd34c3d
AndrewRBurge <burgean@sheridancollege.ca>
2025-04-09 14:07:45 -0400

    -fixing comments and adding user input to saves
    -added more comments
    -added user input to save data function
    -minor bug fixing/ spellchecking

--- Group Project/Group Project/DataToDisk.c
+++ Group Project/Group Project/DataToDisk.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include"DataToDisk.h"
 #include "List.h"
-
+// following functions created by Andrew Burge
 
 void LoadTaskDataFromFile() {
 
     printf("data loaded\n");
-   
+    FILE* file = fopen("TasksData.dat", "rb");
+    if (file == NULL) {
+        printf("No saved tasks found, starting fresh\n");
+        return;
+    }
 	
 	/*
     	
-	      FILE *file = fopen("tasks.dat", "rb");
-    if (file == NULL) {
-        printf("No saved tasks found, starting fresh.\n");
-        return;
-    }
+	//basiclly anything that fits into a contair of a task is being read we add that data to the list as a task
 
     TASK t;
     while (fread(&t, sizeof(TASK), 1, file)) {
@@ -34,26 +34,35 @@ void LoadTaskDataFromFile() {
 
 
 void SaveTaskDataToFile(PLISTNODE list) {
-	
-    printf("data saved\n");
-	
-	/*
-      FILE *file = fopen("tasks.dat", "wb");
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file for writing\n");
-        return;
-    }
+    int savecounter = 0;
+    //printf("data saved\n");
+    printf("would like to save your data to disk\nplease input a number below\n1) yes\n2) no\n");
+      int numsymbols = scanf("%d", &savecounter); 
+        if (savecounter == 1) {
 
-    PLISTNODE current = list;
-    while (current != NULL) {
-        fwrite(&current->data, sizeof(TASK), 1, file);
-        current = current->next;
-    }
+            /*
+                  FILE *file = fopen("TasksData.dat", "wb");
+                if (file == NULL) {
+                    fprintf(stderr, "Error opening file for writing\n");
+                    return;
+                }
 
-    fclose(file);
-    printf("Tasks saved to disk successfully.\n");
-    
-    */
-	
+                PLISTNODE current = list;
+                while (current != NULL) {
+                    fwrite(&current->data, sizeof(TASK), 1, file);
+                    current = current->next;
+                }
+
+                fclose(file);
+                printf("Tasks saved to disk successfully.\n");
+
+                */
+
+        }
+        else if (savecounter == 2) {
+            printf("data not saved\n");
+            return;
+
+        }
 
 }
